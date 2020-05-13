a76b81f7ec18c1b740ba673c8513452aa178de52
Margot13 <mchalland@etu.isara.fr>
2020-05-13 17:56:15 +0200

    patat

--- Anova.R
+++ Anova.R
@@ -1,16 +1,18 @@
 library("agricolae")
 
 ### Import
-data <- read.table("activableR19.csv", header = TRUE, sep = ",")
-dim(data)
-names(data)
-table(data$variete)
+donnes <- read.table("activableR19.csv", header = TRUE, sep = ",")
+dim(donnes)
+names(donnes)
+table(donnes$variete)
+
 
 
 
 ### Variables
-rdt <- data$rendement
-fac <- as.factor(data$variete)
+rdt <- donnes$rendement
+fac <- as.factor(donnes$variete)
+
 
 
 
@@ -18,10 +20,6 @@ fac <- as.factor(data$variete)
 
 moyennes <- tapply(rdt,fac,mean) # afficher
 variances <- tapply(rdt,fac,var) # afficher
-
-
-
-### Graphique
 plot <- boxplot(rdt~fac, ylab="rendement en qx/ha") # afficher
 
   
@@ -29,27 +27,32 @@ plot <- boxplot(rdt~fac, ylab="rendement en qx/ha") # afficher
 
 ### Démontrer les 3 hypothèse 
 
+#Normalité
+shapiro.test(lm1$residuals) ## (Affiche) Ok si P-Value > 0,05.
+
+#homoscédasticité
+bartlett.test(rdt~fac) ## (Affiche) OK si P-Value > 0,05.
+
 #indépendance
 lm1 <- lm(rdt~fac)
 par(mfrow=c(2,2))
-plotsindep <- plot(lm1)## (Demande à l'utilisateur) Ok si  : - Residual, Scale-Location et Constante Levergae : points éparpillés, - Normal Q_Q : points sur la ligne, 
+plotsindep <- plot(lm1) ## (Demande à l'utilisateur + Affiche) Ok si  : - Residual, Scale-Location et Constante Levergae : points éparpillés, - Normal Q_Q : points sur la ligne, 
 
-#Normalité
-shapiro.test(lm1$residuals) ## (Affiche) Ok si P-Value > 0,05
 
-#homoscédasticité
-bartlett.test(rdt~fac) ## (Affiche) OK si P-Value > 0,05
 
 #Si les 3 conditions d'hypothèse ne sont pas validées, on ne peut pas faire d'ANOVA, demander si il veut quand même la faire.
 
 
 
 
+
 ### Table d'ANOVA
+
 anova(lm(rdt~fac)) ## Afficher 
 
 # Si P-Value > 0,05, alors on conserve l'hypothèse H0, on n'a pas pu mettre en évidence l'effet du facteur testé.
 # Si P-Value < 0,05 alors on rejete H0, on a pu mettre en évidence un effet du facteur testé.
+
 
 
 
@@ -60,10 +63,11 @@ anova(lm(rdt~fac)) ## Afficher
 TukeyHSD(aov(rdt~fac))
 plot(TukeyHSD(aov(rdt~fac)))
 
-# Si P-Value <0,05, alors on fait le teste de Tuckey, si non demander si veut le faire quand même.
+# Si P-Value <0,05, alors on fait le teste de Newman-Keuls, si non demander si veut le faire quand même.
 
-#df<-df.residual(aov(rdt~fac, data))
-#MSerror<-deviance(aov(rdt~fac, data))/df
-#out <- with(data,SNK.test(aov(rdt~fac, data), rdt, group=FALSE,))
-#print(out$groups)
-#print(SNK.test(aov(rdt~fac, data), rdt, group=FALSE,))
+ble <- read.table("activableR19.csv", header = TRUE, sep = ",")
+
+model<-aov(rendement~variete,data = donnes)
+out <- SNK.test(model,"variete", console=TRUE, 
+                main="patata")
+print(SNK.test(model,"variete", group=FALSE))
